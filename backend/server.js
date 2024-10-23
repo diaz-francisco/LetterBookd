0f05bea99b01a4d4944b5dde4f477195af57c257
MrCheks <fazeopticdiaz@gmail.com>
2024-10-23 18:53:56 -0500

    Frontend and Backend directories established

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3001;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
---
+++ frontend/src/App.tsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3002")
+      .then((response) => {
+        setMessage(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the data!", error);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{message}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
