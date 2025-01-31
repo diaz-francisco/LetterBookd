939ccdb59ec649b5bbec97cdd39075f974448293
MrCheks <fazeopticdiaz@gmail.com>
2025-01-31 16:31:55 -0600

    Header CSS and Component fix

--- frontend/src/components/LandingPage/Header.tsx
+++ frontend/src/components/LandingPage/Header.tsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 
 const Header: React.FC = () => {
+  // const [search, setSearch] = useState("");
+
   return (
     <header className="header">
       <h1></h1>
       <nav>
         <ul>
           <li>
-            <a href="#1">Home</a>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#monthly-book">
+              Monthly Book
+            </a>
+          </li>
+          <li>
+            <form>
+              <input
+                type="text"
+                placeholder="Search..."
+              ></input>
+            </form>
+          </li>
+          <li>
+            <a href="#sign-up">Sign Up</a>
+          </li>
+          <li>
+            <a href="#login">Login</a>
           </li>
         </ul>
       </nav>
