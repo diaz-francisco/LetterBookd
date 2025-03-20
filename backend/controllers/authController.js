d539320d937c307af7f38206dedcff81604061b6
MrCheks <fazeopticdiaz@gmail.com>
2025-03-20 13:06:12 -0500

    Secret and Payload

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,16 @@ const User = require("./../models/userModel.js");
 const catchAsync = require("./../utils/catchAsync");
 const jwt = require("jsonwebtoken");
 
+const {
+  DefaultAzureCredential,
+} = require("@azure/identity");
+const {
+  SecretClient,
+} = require("@azure/keyvault-secrets");
+
+const keyVaultUrl =
+  "https://<your-keyvault-name>.vault.azure.net/";
+
 exports.signup = catchAsync(
   async (req, res, _next) => {
     const newUser = await User.create(req.body);
