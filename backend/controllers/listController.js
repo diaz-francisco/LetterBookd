d5d46417c80c8826182bcfb03ea28d5600c0583b
Francisco Diaz <frank@Franciscos-MacBook-Air.local>
2025-06-05 16:37:50 -0500

    List populate

--- backend/controllers/listController.js
+++ backend/controllers/listController.js
@@ -1,9 +1,18 @@
-const catchAsync = require("../utils/catchAsync");
 const List = require("../models/listModel");
+const catchAsync = require("../utils/catchAsync");
 
-exports.getLists = catchAsync(async (req, res, next) => {
-  const lists = await List.find().populate({
-    path: "user",
-    select: "name photo",
+exports.getAllLists = catchAsync(async (req, res, next) => {
+  const lists = await List.find().populate({ path: "user", select: "name photo" });
+
+  res.status(200).json({
+    status: "Success",
+    results: lists.length,
+    data: {
+      lists,
+    },
   });
 });
+
+exports.createList = catchAsync(async (req, res, next) => {});
+
+module.exports;
