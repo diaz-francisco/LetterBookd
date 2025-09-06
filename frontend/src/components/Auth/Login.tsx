4902110fb61551f1d99e858fc538adb4620e2441
MrCheks <fazeopticdiaz@gmail.com>
2025-09-05 19:04:18 -0500

    Login component

--- frontend/src/components/Auth/Login.tsx
+++ frontend/src/components/Auth/Login.tsx
@@ -1,31 +1,5 @@
-// import axios from "axios";
-// import React, { useState } from "react";
+import axios from axios
 
-// const Login: React.FC = () => {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-
-//   async function handleLogin(e: React.FormEvent) {
-//     e.preventDefault();
-
-//     try {
-//       const res = await axios.post(
-//         "http://127.0.0.1:3001/api/v1/users/login",
-//         { email, password },
-//         { withCredentials: true }
-//       );
-
-//       if (res.status === 200) {
-//         console.log("successfull");
-//       }
-//     } catch (err: any) {
-//       console.error(err);
-//     }
-//   }
-
-//   return (
-//     <>
-//       <div></div>
-//     </>
-//   );
-// };
+export default function Login(){
+    
+}
