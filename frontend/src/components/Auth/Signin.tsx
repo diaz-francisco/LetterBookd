154ddfa84ba95bccac153afde53a7b92738fdfe0
Francisco Diaz <frank@Franciscos-MacBook-Air.local>
2025-09-09 12:24:33 -0500

    Css

--- frontend/src/components/Auth/Signin.tsx
+++ frontend/src/components/Auth/Signin.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import "./styles/Signin.css";
 
 const Signin: React.FC = () => {
   const [mode, setMode] = useState<"signin" | "signup">("signin");
 
   return (
-    <div>
-      <div>
+    <div className="modal">
+      <div className="buttons">
         <button aria-pressed={mode === "signin"} onClick={() => setMode("signin")}>
           Sign In
         </button>
@@ -15,17 +16,24 @@ const Signin: React.FC = () => {
       </div>
       {/* Mode Switch */}
       {mode === "signin" ? (
-        <form>
+        <form className="modal-content">
           <input type="email" name="email" placeholder="Email" autoComplete="email" required />
           <input type="password" name="password" placeholder="Password" autoComplete="password" required />
-          <button type="submit">Sign In</button>
+          <button type="submit">Submit</button>
           <div>
             <a href="/forgot-password">Forgot Password</a>
             <button type="button">Continue with google</button>
+            <input type="checkbox"></input>
+            <p>Remember me</p>
           </div>
         </form>
       ) : (
-        <form></form>
+        <form className="modal-content">
+          <input type="email" name="email" placeholder="Email" autoComplete="email" required />
+          <input type="password" name="password" placeholder="Password" autoComplete="password" required />
+          <input type="password" name="password" placeholder="Re-enter Password" autoComplete="password" required />
+          <button type="button">Submit</button>
+        </form>
       )}
     </div>
   );
