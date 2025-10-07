26374fd2f72726b5663a628598b0718643817e6d
Francisco Diaz <frank@Franciscos-MacBook-Air.local>
2025-10-07 12:19:59 -0500

    Book details api

--- frontend/src/hooks/useFetchBookDetails.ts
+++ frontend/src/hooks/useFetchBookDetails.ts
@@ -21,4 +21,23 @@ export function useFetchBookDetails(bookId: string) {
   const [loading, setLoading] = useState(false);
   const [book, setBooks] = useState<BookDetails | null>(null);
   const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!bookId) return;
+
+    async function useFetchBookDetails() {
+      setLoading(true);
+      setError(null);
+
+      const bookRes = await fetch(`https://openlibrary.org/works/${bookId}.json`);
+
+      if (!bookRes.ok) {
+        throw new Error(`Book not found: ${bookRes.status}`);
+      }
+
+      const bookData = await bookRes.json();
+
+      let authorName = "Unknown Author";
+    }
+  });
 }
